e2ba88afadca7c9da820567cf8218928bcce8846
Evgenii <eugenysvirin@gmail.com>
2024-03-18 22:02:49 +0300

    add: hw2 task3

---
+++ hw2/artifacts/3/Readme.md
@@ -0,0 +1,6 @@
+В контейнер скопипастить main.py (или по-нормальному через pull)
+
+```
+mv main.py home/main.py
+python3 home/main.py
+```
--- hw2/artifacts/3/main.py
+++ hw2/artifacts/3/main.py
@@ -4,14 +4,15 @@ if __name__ == "__main__":
     table1 = []
     table2 = [[1, 2, 3]]
     table3 = [[1, 2, 3],
-              [4, 5, 6]]
+            [4, 5, 6]]
 
     image = "mug.png"
-
-    filename = "../artifacts/2/image_table.tex"
+    filename = "image_table.tex"
     with open(filename, "w") as file:
         tex_elements = tex.create_tex_table(table3)
         tex_elements += tex.create_tex_image(image)
         file_content = tex.wrap_with_tex_document(tex_elements)
         file.write(file_content)
-    tex.create_pdf_file(filename, "../artifacts/2/image_table")
+        file.close()
+    tex.create_pdf_file(filename, "image_table")
+
